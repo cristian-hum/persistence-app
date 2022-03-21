de3ecc38cbea2439741dda560350b856aee595b8
Cristian Humelnicu <cristian@tehnicunion.com>
2022-03-21 08:55:11 +0200

    Implement basic in memory db

--- jdbc/src/main/java/com/fm/jdbc/book/BookJdbcInMemoryDao.java
+++ jdbc/src/main/java/com/fm/jdbc/book/BookJdbcInMemoryDao.java
@@ -1,6 +1,7 @@
 package com.fm.jdbc.book;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 // TODO: implement this using jdbc prepared statements
 // use a map as an in-memory database
@@ -10,37 +11,50 @@ public class BookJdbcInMemoryDao implements BookRepository {
 
     @Override
     public Book create(Book book) {
-        return null;
+        Long newId = Long.valueOf(db.size());
+        book.setId(newId);
+        db.put(newId, book);
+        return book;
     }
 
     @Override
     public List<Book> findAll() {
-        return null;
+        return new ArrayList<>(db.values());
     }
 
     @Override
     public List<Book> findByAuthor(String author) {
-        return null;
+        return db.values()
+                .stream()
+                .filter(book -> book.getAuthor() == author)
+                .toList();
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        return null;
+        return db.values()
+                .stream()
+                .filter(book -> book.getId() == id)
+                .findFirst();
     }
 
     @Override
     public Optional<Book> findByTitle(String title) {
-        return null;
+        return db.values()
+                .stream()
+                .filter(book -> book.getTitle() == title)
+                .findFirst();
     }
 
     @Override
     public Book update(Long id, Book book) {
-        return null;
+        db.replace(id, book);
+        return book;
     }
 
     @Override
     public void delete(Long id) {
-
+        db.remove(id);
     }
 
 }
